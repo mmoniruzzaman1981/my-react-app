2a58d46f8cf01a0db5574d74ee894e63a82c5a0f
Md. Moniruzzaman <moniruzzaman@Mds-MacBook-Pro.local>
2021-02-24 09:58:26 +0600

    component

--- src/App.js
+++ src/App.js
@@ -5,15 +5,22 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit Done <code>src/App.js</code> and save to reload.
-        </p>
-        <h1>My Heading</h1>
-        <p>My First React Paragraph</p>
+      <p>My First React, I am a React Person</p>
+      <Person name = 'Abir' job = 'football' ></Person>
+      <Person name = 'Kabir' job = 'tenis' ></Person>
+      <Person name = 'Jabir' job = 'Badminton' ></Person>
       </header>
     </div>
   );
+}
+
+function Person(props){
+  return (
+    <div style= {{border: '2px solid gold', width: '400px', margin: '10px'}} >
+      <h3>My Name: {props.name} </h3>
+      <p>My Profession: {props.job} </p>
+    </div>
+  )
 }
 
 export default App;
